d07e074e3adcc0a2e38b7f3e75746f4a6fac336a
prashant-659 <parshantbhatia659@gmail.com>
2022-12-27 22:44:32 +0530

    message

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #rest_frame
+    'rest_framework',
     #Local
-    "todos.apps.TodosConfig",
+    'todos.apps.TodosConfig',
 ]
+Rest_Framework={
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny",
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
